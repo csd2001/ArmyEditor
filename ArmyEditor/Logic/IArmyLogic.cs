20aaaf6b94ac501440163450776259d29b7d389e
Csákány Dominik <114937381+csd2001@users.noreply.github.com>
2023-03-27 17:20:00 +0200

    Add project files.

---
+++ ArmyEditor/Logic/IArmyLogic.cs
@@ -0,0 +1,15 @@
+﻿using ArmyEditor.Models;
+using System.Collections.Generic;
+
+namespace ArmyEditor.Logic
+{
+    public interface IArmyLogic
+    {
+        int AllCost { get; }
+
+        void AddToArmy(Trooper trooper);
+        void EditTrooper(Trooper trooper);
+        void RemoveFromArmy(Trooper trooper);
+        void SetupCollections(IList<Trooper> barracks, IList<Trooper> army);
+    }
+}
---
+++ ArmyEditor/Models/Trooper.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyEditor.Models
+{
+    public class Trooper : ObservableObject
+    {
+        private string type;
+        public string Type
+        {
+            get { return type; }
+            set { SetProperty(ref type, value); }
+        }
+
+        private int power;
+        public int Power
+        {
+            get { return power; }
+            set { SetProperty(ref power, value); }
+        }
+
+        private int vitality;
+        public int Vitality
+        {
+            get { return vitality; }
+            set { SetProperty(ref vitality, value); }
+        }
+        private int cost;
+        public int Cost
+        {
+            get { return cost; }
+            set { SetProperty(ref cost, value); }
+        }
+
+
+
+        public Trooper GetCopy()
+        {
+            return new Trooper()
+            {
+                Type = this.Type,
+                Power = this.Power,
+                Vitality = this.Vitality,
+                Cost = this.Cost
+            };
+        }
+
+
+    }
+}
